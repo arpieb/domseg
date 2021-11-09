c57baad081d7f4a53c236cde355756e4f9e54f96
arpieb <rob@octobang.com>
2021-11-09 14:46:55 -0500

    Started migration of proto.html into sidebar extension components

--- extension/sidebar/sidebar.js
+++ extension/sidebar/sidebar.js
@@ -1,33 +1,101 @@
-{
-  var selectedClassButton = null;
-  document.querySelectorAll('.class-selector').forEach(item => {
-    item.addEventListener('click', function(e) {
-      selectedClassButton = e.currentTarget;
-      updateSelected();
-    })
-  })
+// Define and init application state
+var state = {
+  curClass: null,
+  curElement: null,
+  taggingToggle: document.getElementById('taggingActive'),
+  resetButton: document.getElementById('reset'),
+  tagClasses: {},
+}
 
-  function updateSelected() {
+// Test if we can tag the currently active element
+function taggableElement(e) {
+  target = e.target;
+  casedTagName = target.tagName.toLowerCase();
+  if (state.taggingToggle.checked &&
+    state.selectedClassButton !== null &&
+    casedTagName !== 'html' && target !== casedTagName !== 'body'
+    ) return true;
+  return false;
+}
+
+// Update the document element backgrounds based on mouseover update
+function updateBackgrounds(target) {
+  prevElement = state.curElement;
+  state.curElement = target;
+
+  if (prevElement !== null) {
+    if (!prevElement.dataset.heaTaggedClass) {
+      prevColor = prevElement.dataset.heaPrevBgcolor;
+      delete prevElement.dataset.heaPrevBgcolor;
+    }
+    else {
+      prevColor = state.tagClasses[prevElement.dataset.heaTaggedClass];
+    }
+    prevElement.style.backgroundColor = prevColor;
+  }
+  if (state.curElement !== null) {
+    selectedClassColor = state.tagClasses[state.curClass];
+    if (!state.curElement.dataset.heaTaggedClass) {
+      state.curElement.dataset.heaPrevBgcolor = state.curElement.style.backgroundColor;
+    }
+    state.curElement.style.backgroundColor = selectedClassColor;
+  }
+}
+
+// Tag element with currently selected data class and do something with it
+function tagElement(el) {
+  tagClass = state.curClass;
+  el.dataset.heaTaggedClass = tagClass;
+  data = {
+    docInfo: document,
+    html: document.getRootNode().children[0].outerHTML,
+  };
+  console.log('tagged', tagClass);
+  //console.log(JSON.stringify(data));
+}
+
+// Register click handler for each tag class button in sidebar
+document.querySelectorAll('.class-selector').forEach(item => {
+  state.tagClasses[item.dataset.class] = item.style.backgroundColor;
+  item.addEventListener('click', function (e) {
+    state.curClass = e.currentTarget.dataset.class;
     document.querySelectorAll('.class-selector').forEach(item => {
       item.classList.remove('selected');
     })
-    selectedClassButton.classList.toggle('selected');
-    //document.getElementById('selected-class').textContent = selectedClassButton.style.backgroundColor;
-  }
-
-  const curTab = browser.tabs.getCurrent();
-  var curElement = null;
-  curTab.document.addEventListener('mouseover', function(e) {
-    const attrColor = 'data-hea-prev-bgcolor'
-    prevElement = curElement;
-    curElement = e.currentTarget;
-
-    if (prevElement !== null) {
-      prevColor = prevElement.attributes[attrColor];
-      prevElement.style.backgroundColor = prevColor;
-    }
-    selectedClassColor = selectedClassButton.style.backgroundColor;
-    curElement.attributes[attrColor] = curElement.style.backgroundColor;
-    curElement.style.backgroundColor = selectedClassColor;
+    e.target.classList.toggle('selected');
   })
-}
+});
+
+// Add click handler for reset button
+state.resetButton.addEventListener('click', function (e) {
+  if (state.taggingToggle.checked) {
+    Array.from(document.all).forEach(item => {
+      delete item.dataset.heaTaggedClass;
+      item.style.backgroundColor = item.dataset.heaPrevBgcolor;
+      delete item.dataset.heaPrevBgcolor;
+    });
+  }
+});
+
+// Add click handler to "activate tagging" checkbox to toggle state of reset button
+state.taggingToggle.addEventListener('click', function (e) {
+  state.resetButton.disabled = state.taggingToggle.checked ? false : true;
+});
+
+// Add mouseover handler for taggable page elements
+// document.addEventListener('mouseover', function (e) {
+//   if (taggableElement(e)) {
+//     updateBackgrounds(e.target);
+//   }
+//   else {
+//     updateBackgrounds(null);
+//   }
+// });
+
+// Add click handler for taggable page elements
+// document.addEventListener('click', function (e) {
+//   if (taggableElement(e)) {
+//     tagElement(e.target);
+//     e.preventDefault();
+//   }
+// });
